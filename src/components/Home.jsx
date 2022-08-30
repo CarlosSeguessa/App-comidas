dbb47b830e064e9dd6f4a461496f7fd2969cc158
Carlos <carlosseguessa@gmail.com>
2022-08-30 12:23:17 -0300

    First commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+      <>
+        <h1 className='text-3xl text-center text-white'>Favoritos</h1>
+        <h2 className='text-2xl text-center text-white'>Oops!! Parece que no has agregado a favoritos ningún restaurante</h2>
+      </>  
+    );
+}
+
+export  {Home};
